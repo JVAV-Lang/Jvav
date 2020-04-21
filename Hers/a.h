db18d7595a0487347cb5c622e308068b89d96cce
东灯 <43312495+Lampese@users.noreply.github.com>
2020-04-21 20:46:09 +0800

    Add files via upload

---
+++ Hers/a.h
@@ -0,0 +1,33 @@
+inline int Addition(int num1,int num2){
+	int x=num1^num2;
+	int y=num1&num2;
+	while(y!=0){
+		y=y<<1;
+		int temp=x;
+		x=x^y;
+		y=temp&y;
+	}
+	return x;
+}
+inline long Addition(long num1,long num2){
+	long x=num1^num2;
+	long y=num1&num2;
+	while(y!=0){
+		y=y<<1;
+		long temp=x;
+		x=x^y;
+		y=temp&y;
+	}
+	return x;
+}
+inline long long Addition(long long num1,long long num2){
+	long long x=num1^num2;
+	long long y=num1&num2;
+	while(y!=0){
+		y=y<<1;
+		long long temp=x;
+		x=x^y;
+		y=temp&y;
+	}
+	return x;
+}
