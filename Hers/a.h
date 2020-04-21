d872b0dcb3e6e0f6f2e760bc6403ca3b7d58fb1e
东灯 <43312495+Lampese@users.noreply.github.com>
2020-04-21 21:15:32 +0800

    Add files via upload

--- Hers/a.h
+++ Hers/a.h
@@ -31,3 +31,33 @@ inline long long Addition(long long num1,long long num2){
 	}
 	return x;
 }
+inline int Subtraction(int num1,int num2){
+	int x=num1^num2;
+	int y=x&num2;
+	while(y!=0){
+		y=y<<1;
+		x=x^y;
+		y=x&y;
+	}
+	return x;
+}
+inline long Subtraction(long num1,long num2){
+	long x=num1^num2;
+	long y=x&num2;
+	while(y!=0){
+		y=y<<1;
+		x=x^y;
+		y=x&y;
+	}
+	return x;
+}
+inline long long Subtraction(long long num1,long long num2){
+	long long x=num1^num2;
+	long long y=x&num2;
+	while(y!=0){
+		y=y<<1;
+		x=x^y;
+		y=x&y;
+	}
+	return x;
+}
